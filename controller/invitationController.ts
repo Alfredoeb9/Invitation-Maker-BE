f7d9f5ba0cf9bdff0996f8c5dd48928635868131
Alfredoeb9 <alfredoeb96@gmail.com>
2024-05-28 18:58:41 -0700

    [invitation] updated error if duplicate name

--- controller/invitationController.ts
+++ controller/invitationController.ts
@@ -4,27 +4,26 @@ import { invitations, invitationsForm } from "../db/schema";
 
 export const createInv = async (req: Request, res: Response) => {
   try {
-    // console.log("req", req);
     const { name, description, email } = req.body;
-    // const id = req.headers;
-
-    // console.log("id", id);
-    console.log("req", req);
-    // console.log("id", id);
 
     const formIdUUID = crypto.randomUUID();
     const invitationUUID = crypto.randomUUID();
 
     await db
+      .insert(invitations)
+      .values({ id: formIdUUID, name: name, createdBy: email, description })
+      .returning();
+    await db
       .insert(invitationsForm)
       .values({ id: invitationUUID, formId: formIdUUID });
 
-    await db
-      .insert(invitations)
-      .values({ id: formIdUUID, name: name, createdBy: email, description });
-
     return res.status(201).json({ message: "New invitation created" });
   } catch (error) {
+    if (
+      error.message.includes("UNIQUE constraint failed: inv_invitations.name")
+    ) {
+      error.message = "Name is taken in your repository";
+    }
     return res.status(400).json({ error: error.message });
   }
 };
